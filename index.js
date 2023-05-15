1e23b9577da879f20b2f14752207e0395477a64a
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-16 00:09:42 +0600

    verify token

--- index.js
+++ index.js
@@ -20,7 +20,24 @@ const client = new MongoClient(uri, {
   },
 });
 
-const verify
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
